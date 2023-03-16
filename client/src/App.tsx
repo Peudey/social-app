4db5d0a01a097e8f61ee5606af365ed19c53437a
Parker Eudey <parkereudey@gmail.com>
2023-03-15 22:38:55 -0500

    moved client to client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/App.css';
+import Post from './components/post';
+
+function App() {
+  return (
+    <div className="App">
+      <Post />
+    </div>
+  );
+}
+
+export default App;
