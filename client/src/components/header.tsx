dc1c9503ade31cc589dcae2c15d1b5cb17cce411
Parker Eudey <parkereudey@gmail.com>
2023-03-16 13:17:36 -0500

    basic header

---
+++ client/src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <a href='/'>Social App</a>
+    </div>
+  );
+}
+
+export default Header;
--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './header';
 
 const Home = ({children}:any) => {
   return (
     <div className="App">
+        <Header />
       {children}
     </div>
   );
