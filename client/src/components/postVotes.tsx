78373cc11a01c55f3b6cc58ecb6f356b73d862b8
Parker Eudey <parkereudey@gmail.com>
2023-03-21 22:37:56 -0500

    fixed vote score

--- client/src/components/postVotes.tsx
+++ client/src/components/postVotes.tsx
@@ -3,31 +3,30 @@ import { BiUpArrowAlt, BiDownArrowAlt } from "react-icons/bi";
 
 function PostVotes(props: {score: number}) {
     let {score} = props;
-    const [voteCount, setVoteCount] = useState(score);
     const [vote, setVote] = useState("");
 
     const upVoteHandler = (event:any) => {
         if(vote==="") {
-            setVoteCount(voteCount+1);
+            score += 1;
             setVote("up");
         } else if (vote==="up") {
-            setVoteCount(voteCount-1);
+            score -= 1;
             setVote("");
         } else if (vote==="down") {
-            setVoteCount(voteCount+2);
+            score += 2;
             setVote("up");
         }
     }
 
     const downVoteHandler = (event:any) => {
         if(vote==="") {
-            setVoteCount(voteCount-1);
+            score -= 1;
             setVote("down");
         } else if (vote==="down") {
-            setVoteCount(voteCount+1);
+            score += 1;
             setVote("");
         } else if (vote==="up") {
-            setVoteCount(voteCount-2);
+            score -= 2;
             setVote("down");
         }
     }
@@ -37,7 +36,7 @@ function PostVotes(props: {score: number}) {
             <BiUpArrowAlt className="vote" size={40} onClick={upVoteHandler}/>:
             <BiUpArrowAlt className="vote" id="upVote" size={40} onClick={upVoteHandler}/>
         }
-        <p>{voteCount}</p>
+        <p>{score}</p>
         {vote!=="down"?
             <BiDownArrowAlt className="vote" size={40} onClick={downVoteHandler}/>:
             <BiDownArrowAlt className="vote" id="downVote" size={40} onClick={downVoteHandler}/>
