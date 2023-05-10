f4e8693673713945a4c43e3419e73a83847993e2
peudey <parkereudey@gmail.com>
2023-05-10 12:07:46 -0500

    added collapse comment

--- client/src/components/comments/comment.tsx
+++ client/src/components/comments/comment.tsx
@@ -3,27 +3,33 @@ import commentTypes from './types';
 import { Link, useParams } from 'react-router-dom';
 
 const Comment = (props:commentTypes) => {
+    const[showChildren, setShown] = useState(true);
     const {username, id, pid, cid, body, children} = props;
-        return (
-            <div className="parentComment">
-                <div className="comment">
-                    <div className="commentBody">
-                        <span className='commentTop'>
-                            <p>posted by </p>
-                            <Link to={`/user/${username}`}>{username}</Link>
-                        </span>
-                        <p>{body}</p>
-                    </div>
+
+    const clickhandler = () => {
+        setShown(!showChildren);
+    }
+
+    return (
+        <div className="parentComment">
+            <div className="comment">
+                <div className="commentBody">
+                    <span className='commentTop' onClick={clickhandler}>
+                        <p>posted by </p>
+                        <Link to={`/user/${username}`}>{username}</Link>
+                    </span>
+                    <p className={showChildren?'shown':'hidden'}>{body}</p>
                 </div>
-                <ul>
-                    {children.map(({username, body, id, pid, cid, children}) => (
-                            <li key={id}>
-                                <Comment body={body} id={id} username={username} pid={pid} cid={cid} children={children}/>
-                            </li>
-                    ))}
-                </ul>
             </div>
-          );
+            <ul className={showChildren?'shown':'hidden'}>
+                {children.map(({username, body, id, pid, cid, children}) => (
+                        <li key={id}>
+                            <Comment body={body} id={id} username={username} pid={pid} cid={cid} children={children}/>
+                        </li>
+                ))}
+            </ul>
+        </div>
+        );
   }
   
   export default Comment;
